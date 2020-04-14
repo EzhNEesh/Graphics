547ec30ac53fe83e52f5f766b57aacdad202001e
EzhNEesh <62452675+EzhNEesh@users.noreply.github.com>
2020-04-14 14:52:19 +0300

    Update Source.cpp

--- lab1/Source.cpp
+++ lab1/Source.cpp
@@ -6,31 +6,56 @@
 
 using namespace std;
 
+enum kommand
+{
+	negative = 0,
+	HorMir = 1,
+	VerMir = 2,
+	ClockwiseRotate = 3,
+	CounterClockwiseRotate = 4
+};
+
 int main(int argc, char* argv[])
 {
+	if (argc != 4)
+	{
+		cout << "ERROR: Wrong amount of arguments\n";
+		return -1;
+	}
 	PNM A;
 	A.input(argv[1]);
-	int kom = atoi(argv[3]);
+	if (A.error)
+	{
+		cout << "ERROR: " << A.error_type << '/n';
+		return -1;
+	}
+	kommand kom;
+	kom = (kommand)atoi(argv[3]);
 	switch (kom)
 	{
-	case (0):
+	case negative:
 		A.negative(argv[2]);
 		break;
-	case (1):
+	case HorMir:
 		A.HorMir(argv[2]);
 		break;
-	case(2):
+	case VerMir:
 		A.VerMir(argv[2]);
 		break;
-	case(3):
+	case ClockwiseRotate:
 		A.ClockwiseRotate(argv[2]);
 		break;
-	case(4):
+	case CounterClockwiseRotate:
 		A.CounterClockwiseRotate(argv[2]);
 		break;
 	default:
-		cout << "Uknown command";
-		break;
+		cout << "ERROR: Uknown command\n";
+		return -1;
+	}
+	if (A.error)
+	{
+		cout << "ERROR: " << A.error_type << '/n';
+		return -1;
 	}
 	return 0;
 }
